f790ada45fc8447bdc73b94fc1bd0d89a1e464a1
Fiki Dzulfiqar <33850678+fikidz@users.noreply.github.com>
2018-10-02 18:34:35 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,22 @@
-<?php
-
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form Login</title>
+</head>
+<body>
+	<form action="registrasi.php" method="POST">
+		<table align="center" bgcolor="lightgreen">
+			<tr align="center">
+				<td colspan="2" bgcolor="lightblue"><h2>Halaman Login</h2></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="username" placeholder="Masukkan Username"></td>
+				<td><input type="password" name="password" placeholder="Masukkan Password"></td>
+			</tr>
+			<tr align="center">
+				<td colspan="2"><input type="submit" value="Login"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
