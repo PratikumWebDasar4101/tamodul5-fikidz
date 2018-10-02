1b3fca984dda014c3e584d334a8c5d37fbebbd26
Fiki Dzulfiqar <33850678+fikidz@users.noreply.github.com>
2018-10-02 18:35:11 +0700

    Update prosesLogin.php

--- prosesLogin.php
+++ prosesLogin.php
@@ -1,7 +1,31 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<body>
+	<h1 align="center">Selamat Datang</h1>
+</body>
+</html>
 
+<?php  
+	session_start();
+	$login = array(
+		"nama"=> "fikidz",
+		"pass" => 12345
+	);
 
+	if ($_POST["username"]==$login["nama"]AND
+		$_POST["password"]==$login["pass"]
+)
 
+	{
+	$_SESSION["nama"]= $_POST['username'];
+	$_SESSION["pass"]= $_POST['password'];
 
+?>
+
+<?php  
+}
+else{
+	header("Location: login.php");
+}
 
 ?>
